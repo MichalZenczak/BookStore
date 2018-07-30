c27ef26a4f90833763430153feb3a56254307738
MichalZenczak <zenczak.michal@gmail.com>
2018-07-30 21:53:05 +0200

    saving to db functionality added

--- app/src/main/java/com/example/michal/bookstore/EditorActivity.java
+++ app/src/main/java/com/example/michal/bookstore/EditorActivity.java
@@ -1,5 +1,6 @@
 package com.example.michal.bookstore;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -10,9 +11,11 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.michal.bookstore.data.BookContract.BookEntry;
 
@@ -110,6 +113,7 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         switch (selectedItem){
             case R.id.action_save:
                 saveBook();
+                finish();
                 break;
             case R.id.action_delete:
                 deleteBook();
@@ -125,7 +129,69 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         String supplierNameString = mSupplierNameEditText.getText().toString().trim();
         String supplierPhoneString = mSupplierPhoneEditText.getText().toString().trim();
 
-        //TODO: finish this method
+        if (TextUtils.isEmpty(productNameString)){
+            Toast.makeText(this, R.string.editor_activity_required_fields_missing, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ContentValues values = new ContentValues();
+        values.put(BookEntry.COLUMN_PRODUCT_NAME, productNameString);
+
+        // If the price is not provided by the user, don't try to parse the string into an
+        // integer value. Use 0 by default.
+        int price = 0;
+        if (!TextUtils.isEmpty(priceString)){
+            // check if the value is not negative
+            if (Integer.parseInt(priceString) > 0){
+                price = Integer.parseInt(priceString);
+            }else {
+                Toast.makeText(this, R.string.editor_activity_no_negative_values,Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+        }
+        values.put(BookEntry.COLUMN_PRICE, price);
+
+        // If the quantity is not provided by the user, don't try to parse the string into an
+        // integer value. Use 0 by default.
+        int quantity = 0;
+        if (!TextUtils.isEmpty(quantityString)){
+            // check if the value is not negative
+            if (Integer.parseInt(quantityString) > 0){
+                quantity = Integer.parseInt(quantityString);
+            }
+            else {
+                Toast.makeText(this, R.string.editor_activity_no_negative_values,Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+        }
+        values.put(BookEntry.COLUMN_QUANTITY, quantity);
+
+        values.put(BookEntry.COLUMN_SUPPLIER_NAME, supplierNameString);
+        values.put(BookEntry.COLUMN_SUPPLIER_PHONE_NUMBER, supplierPhoneString);
+
+        //check if the user inputs new book or updates an existing one
+        if (mCurrentBookUri == null){
+            //insert new book
+            Uri newUri = getContentResolver().insert(BookEntry.CONTENT_URI, values);
+            if (newUri == null){
+                Toast.makeText(this, R.string.editor_activity_insert_failed, Toast.LENGTH_SHORT).show();
+            } else{
+                Toast.makeText(this, R.string.editor_activity_insert_successful, Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            //update existing book
+            int rowsUpdated = getContentResolver().update(mCurrentBookUri, values,null,null);
+            // Show a toast message depending on whether or not the update was successful.
+            if (rowsUpdated == 0){
+                // If no rows were affected, then there was an error with the update.
+                Toast.makeText(this, R.string.editor_activity_update_failed, Toast.LENGTH_SHORT).show();
+            }else {
+                // Otherwise, the update was successful and we can display a toast.
+                Toast.makeText(this, R.string.editor_activity_update_successful, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void deleteBook(){
